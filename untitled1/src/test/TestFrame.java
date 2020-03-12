4bdbe5cb63f94a22272e0c0ee969c6f757d73ffc
zgw <javazgw@gmail.com>
2020-03-12 12:32:01 +0800

    no message

---
+++ untitled1/src/test/TestFrame.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020.  javazgw@gmail.com
+ *
+ *
+ */
+
+package test;
+
+import javax.swing.*;
+
+public class TestFrame {
+
+    public static void main(String[] argc)
+    {
+
+
+       // String path = TestFrame.class.getProtectionDomain().getCodeSource().getLocation()+"";
+        System.out.println("path");
+        JFrame jf = new JFrame();
+        jf.setSize(640,480);
+        jf.setVisible(true);
+    }
+}
